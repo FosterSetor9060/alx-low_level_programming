f86860b22c59c267f833889c16a465454a1c9e8d
FosterSetor9060 <walkerdziwornu@gmail.com>
2023-09-26 08:57:08 -0700

    checking code 1-list_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * struct listint_s - more singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+
+typedef struct listint_s
+{
+	int n;
+
+	struct listint_s *next;
+} listint_t;
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+	const listint_t *node = h;
+	while (node != NULL)
+	{
+		counter++;
+		node = node->next;
+	}
+  
+	return counter;
+}

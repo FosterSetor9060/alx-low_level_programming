dcbec16f0e55a3bf4c9f64961bce7b76b442bcc8
FosterSetor9060 <walkerdziwornu@gmail.com>
2023-09-27 02:38:14 -0700

    inscripted main initials

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * pre_main - Prints a message before main is executed.
+ * main_initials - Prints a message before main is executed....
  * __attribute__((constructor))
  */
-void pre_main(void)
+void __attribute__ ((constructor)) main_initials()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
 

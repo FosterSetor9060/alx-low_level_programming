52a11ae7dab4b3f0bceebfcf82293dd4cd983780
FosterSetor9060 <walkerdziwornu@gmail.com>
2023-10-05 03:24:29 -0700

    declarations permissions3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,96 +1,74 @@
 #include "main.h"
 
+#define BUFFER_SIZE 1024
 
-/**
- * main - the entry point always
- * @argc: representents the argument_count
- * @argv: represent the argument_vector
- * Return: 0 on success, error_codes on failure
- */
-int main(int argc, char *argv[])
+int open_source_file(const char *file_from)
 {
-	if (argc != 3)
+	int source_fd = open(file_from, O_RDONLY);
+	if (source_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		return (97);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
-
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-
-	if (copy_file(file_from, file_to) != 0)
-		return (1);
-
-	return (0);
+	return source_fd;
 }
 
-/**
- * copy_file - Copy the content of one file to another
- * @file_from: initial path file
- * @file_to: Destination path fOr the file
- * Return: 0 on success, error codes on failure
- */
-int copy_file(const char *file_from, const char *file_to)
+int open_dest_file(const char *file_to)
 {
-	int source_fd, dest_fd;
+	int dest_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (dest_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		exit(99);
+	}
+	return dest_fd;
+}
+
+void copy_file_contents(int source_fd, int dest_fd)
+{
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
-
-	source_fd = open(file_from, O_RDONLY);
-	if (source_fd == -1)
-		return (error_message(98, file_from, NULL));
-
-	dest_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (dest_fd == -1)
-		return (error_message(99, file_to, NULL));
 
 	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(dest_fd, buffer, bytes_read);
 		if (bytes_written == -1)
-			return (error_message(99, file_to, file_from));
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+			close(source_fd);
+			close(dest_fd);
+			exit(99);
+		}
 	}
 
 	if (bytes_read == -1)
-		return (error_message(98, file_from, NULL));
-
-	if (close(source_fd) == -1 || close(dest_fd) == -1)
-		return (error_message(100, NULL, NULL));
-
-	return (0);
+	{
+		dprintf(STDERR_FILENO, "Error reading from source file\n");
+		close(source_fd);
+		close(dest_fd);
+		exit(98);
+	}
 }
 
-/**
- * error_message - Printing the Error Message to stderr
- * @error_code: code for the error
- * @file1: the first file_name
- * @file2: the second file_name
- * Return: when failed error
- */
-int error_message(int error_code, const char *file1, const char *file2)
+int main(int argc, char *argv[])
 {
-	const char *message;
-
-	switch (error_code)
+	if (argc != 3)
 	{
-	case 97:
-		message = "Usage: %s file_from file_to\n";
-		break;
-	case 98:
-		message = "Error: Can't read from file %s\n";
-		break;
-	case 99:
-		message = "Error: Can't write to file %s\n";
-		break;
-	case 100:
-		message = "Error: Can't close fd\n";
-		break;
-	default:
-		message = "Unknown error\n";
-		break;
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
 	}
 
-	dprintf(STDERR_FILENO, message, argv[0], file1, file2);
-	return (error_code);
+	const char *file_from = argv[1];
+	const char *file_to = argv[2];
+
+	int source_fd = open_source_file(file_from);
+	int dest_fd = open_dest_file(file_to);
+
+	copy_file_contents(source_fd, dest_fd);
+
+	close(source_fd);
+	close(dest_fd);
+
+	return 0;
 }
 

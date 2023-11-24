c0daf0dcd2acf2aa797846d89116b1db9c72994a
Foster Setor <walkerdziwornu@gmail.com>
2023-11-23 21:39:19 -0800

    initial bit manipulation_3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,41 @@
-#include "main.h"
-
 /**
- * binary_to_uint - converting by a binary num To unsigned int.
- * @b: binary num in a string
+ * binary_to_uint - function converted binary_num to  unsigned int.
+ * @b: pointing string containing a binary_num
  *
- * Return: convetring nUm
+ * Return: unsigned int with float value of binsry number, or 0 if error
  */
-binary_to_uint(const char *b) unsigned int
+
+unsigned int binary_to_uint(const char *b)
 {
+	unsigned int conv_numb = 0;
+	int strg_length;
 	int i;
-	unsigned int dec_val = 0;
+	char c;
+	unsigned int bit_value;
 
 	if (!b)
-		return (0);
+		return (0);/* returns 0 if bi is NULL*/
 
-	for (i = 0; b[i]; i++)
+
+	/* calculating the string_lenght(strg_length) in bi*/
+	strg_length = 0;
+	while (b[strg_length] != '\0')
+		strg_length++;
+
+	/* Checking always for '0' and '1' in the string b */
+	for (i = 0; i < strg_length; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		c = b[i];
+		if (c == '0' || c == '1')
+		{
+			/* Calculating the decimal value of currunt bit */
+			bit_value = c - '0';
+
+			conv_numb = (conv_numb << 1) + bit_value;
+		}
+		else
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (dec_val);
+	return (conv_numb);
 }
-

9dd383d16a2a146bbc2942c5f04641bfe2bde737
Foster Setor <walkerdziwornu@gmail.com>
2023-11-27 01:49:23 -0800

    fileIO2 directory update fast trackede

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,122 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void close_elf(int elf);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+
+void print_elf_info(int elf, char *filename);
+void print_class_data_version(unsigned char *e_ident);
+void print_os_abi_abi_version(unsigned char *e_ident);
+void print_type_entry(int elf, unsigned char *e_ident);
+
+/**
+ * main - Entry point of the ELF header program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ * Return: 0 on success, 1 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int elf;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s <ELF file>\n", argv[0]);
+		exit(1);
+	}
+
+	elf = open(argv[1], O_RDONLY);
+	if (elf == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+
+	print_elf_info(elf, argv[1]);
+
+	close_elf(elf);
+	return (0);
+}
+/**
+ * print_elf_info - Print information from the ELF header.
+ * @elf: File descriptor for the ELF file.
+ * @filename: Name of the ELF file.
+ */
+void print_elf_info(int elf, char *filename)
+{
+	Elf64_Ehdr *header;
+	ssize_t r;
+
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(elf);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", filename);
+		exit(98);
+	}
+
+	r = read(elf, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(elf);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", filename);
+		exit(98);
+	}
+
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class_data_version(header->e_ident);
+	print_os_abi_abi_version(header->e_ident);
+	print_type_entry(elf, header->e_ident);
+
+	free(header);
+}
+/**
+ * print_class_data_version - Print class, data encoding, and version.
+ * @e_ident: ELF identification bytes.
+ */
+void print_class_data_version(unsigned char *e_ident)
+{
+	print_class(e_ident);
+	print_data(e_ident);
+	print_version(e_ident);
+}
+/**
+ * print_os_abi_abi_version - Print OS, ABI, and ABI version.
+ * @e_ident: ELF identification bytes.
+ */
+void print_os_abi_abi_version(unsigned char *e_ident)
+{
+	print_osabi(e_ident);
+	print_abi(e_ident);
+}
+/**
+ * print_type_entry - Print ELF type and entry point.
+ * @elf: File descriptor for the ELF file.
+ * @e_ident: ELF identification bytes.
+ */
+void print_type_entry(int elf, unsigned char *e_ident)
+{
+	unsigned int e_type = ((Elf64_Ehdr *)e_ident)->e_type;
+
+	print_type(e_type, e_ident);
+	print_entry(((Elf64_Ehdr *)e_ident)->e_entry, e_ident);
+
+	close_elf(elf);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,16 @@
+Project Title: 0x15. C - File I/O2
+Overview
+This project focuses on File I/O in the C programming language, emphasizing system calls related to file operations. The primary goal is to gain a comprehensive understanding of file descriptors, I/O system calls (open, close, read, write), file permissions, and system calls in general.
+
+Project Timeline
+Start: Nov 27, 2023, 6:00 AM
+End: Nov 28, 2023, 6:00 AM
+Checker Release: Nov 27, 2023, 12:00 PM
+Auto Review at Deadline
+
+This repository contains C programs that demonstrate various aspects of File Input/Output (I/O) using system calls. The goal of this project is to explore file operations, such as reading from and writing to files, using the C programming language and system calls. The programs in this repository cover a range of file-related tasks and concepts.
+
+File I/O is a crucial aspect of many computer programs, allowing them to read data from and write data to files on the file system. This repository provides a hands-on introduction to File I/O in C, emphasizing the use of system calls to perform file-related operations.
+
+Contributing
+Contributions to this repository are welcome! If you have improvements or additional examples related to File I/O in C, please open a pull request. Make sure to follow the existing code style and include appropriate documentation.

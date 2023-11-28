9401912eeab6b5564313f76bf65b69c9c21fab08
Foster Setor <96155042+FosterSetor9060@users.noreply.github.com>
2023-11-28 07:58:24 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,7 +11,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#define BUFFER_SIZE 1024
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);

875fe0041ebabdb72a9475504ceefc1bad7e50c2
Foster Setor <walkerdziwornu@gmail.com>
2024-01-16 14:26:59 -0800

    setor_operand

---
+++ 0x18-dynamic_libraries/operand.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/**
+ *add - adding two integers
+ *@a: first int
+ *@b: second int
+ *Return: addition
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *sub - subtracting two integers
+ *@a: first int
+ *@b: second int
+ *Return: subtraction for all
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *my_div - dividing two integers
+ *@a: first int
+ *@b: second int
+ *Return: dividing all
+*/
+int my_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - mod for two integers
+ *@a: first int
+ *@b: second int
+ *Return: final result
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
+#include <stdlib.h>
+/**
+ *mul - multiplying two integers
+ *@a: first int
+ *@b: second int
+ *Return: multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
